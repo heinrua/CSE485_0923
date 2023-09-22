bf5532cc9c97f8371b4afadb285d911e54d2deec
Hien DT <144339831+heinrua@users.noreply.github.com>
2023-09-23 00:46:37 +0700

    Add files via upload

---
+++ BTTH01_CSE485_ex02/trangchu.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+    <?php 
+        include "layer/tieude.php";
+    ?>
+    <div class="row">
+        <img class="col-12" src="image/poster.png" alt="">
+    </div>
+    <div>
+        <p class="text-primary ">TOP BÀI HÁT YÊU THÍCH</p>
+        <div class="row">
+            <div class="col-md-4">
+                <img src="" alt="">
+            </div>
+        </div>
+    </div>
+    <?php 
+        include "layer/tieude2.php";
+    ?>
+</body>
+</html>
