14985ce87400137d36d364b153f6fed744e73d9d
Hien DT <144339831+heinrua@users.noreply.github.com>
2023-09-25 23:46:10 +0700

    Update login.php

--- BTTH01_CSE485_ex02/login.php
+++ BTTH01_CSE485_ex02/login.php
@@ -30,7 +30,7 @@
             </label>
         </div>
         <div>
-            <button type="submit" class="btn btn-warning text-body">Login</button>
+            <a href="admin/index_admin.php" type="submit" class="btn btn-warning text-body">Login</a>
         </div>
         </div>
 
